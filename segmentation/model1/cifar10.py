attempt to summarize the accuracy and precision. Precision is most likely wrong.

segmentation/model1/cifar10.py
@@ -317,13 +317,15 @@ def accuracy(logits, labels):
   reshaped_labels = tf.reshape(labels,
                               [label_shape[0]*label_shape[1]*label_shape[2]])
 
-  logits_shape =logits.get_shape().as_list()
+  logits_shape = logits.get_shape().as_list()
   reshaped_logits = tf.reshape(logits,
                               [logits_shape[0]*logits_shape[1]*logits_shape[2],
                               logits_shape[3]])
   predictions = tf.argmax(reshaped_logits, dimension=1)
   correct_predictions = tf.equal(predictions, reshaped_labels)
-  accuracy = tf.reduce_mean(tf.cast(correct_predictions, "float"))
+  accuracy = tf.reduce_mean(tf.cast(correct_predictions, "float"), name='accuracy')
+  tf.add_to_collection('accuracy', accuracy)
+
   def tf_count(t, val):
     elements_equal_to_value = tf.equal(t, val)
     as_ints = tf.cast(elements_equal_to_value, tf.int32)
@@ -333,13 +335,37 @@ def accuracy(logits, labels):
   human_pred = tf.equal(predictions,1)
   human_truth = tf.equal(reshaped_labels,1)
   non_human_truth = tf.not_equal(reshaped_labels,1)
+
   tp = tf.logical_and(human_pred, human_truth)
   tp_count = tf.reduce_sum(tf.cast(tp, "float"))
   fp = tf.logical_and(human_pred, human_truth)
   fp_count = tf.reduce_sum(tf.cast(fp, "float"))
+  
   human_precision = tp_count / (tp_count + fp_count)
+  
+  tf.add_to_collection('precision',human_precision)
+
+  # accuracy_op, human_precision_op = _add_accuracy_precision_summaries(accuracy, human_precision)
 
   return accuracy, human_precision
+
+def _add_accuracy_precision_summaries(accuracy, precision):
+  accuracy_averages = tf.train.ExponentialMovingAverage(0.9, name='avg')
+  accuracies = tf.get_collection('accuracy')
+  accuracy_average_op = accuracy_averages.apply(accuracies + [accuracy])
+
+  for a in accuracies + [accuracy]:
+    tf.scalar_summary(a.op.name +' (raw)', a)
+    tf.scalar_summary(a.op.name, accuracy_averages.average(a))
+
+  precision_averages = tf.train.ExponentialMovingAverage(0.9, name='avg')
+  precisions = tf.get_collection('precision')
+  precision_average_op = accuracy_averages.apply(precisions + [precision])  
+
+  for p in precisions + [precisions]:
+    tf.scalar_summary("human_precision (raw)", p)
+    tf.scalar_summary("human_precision", precision_averages.average(p))
+  return accuracy_average_op, precision_average_op
 
 
 def _add_loss_summaries(total_loss):